ed746ad368841d5fdc5df6b2f2fd7ea44d6c5b34
douglas-putnam <douglasputnam@gmail.com>
2018-07-25 10:23:45 -0400

    Add log in and sign up methods to sign in view controller

--- Tinder/LogInViewController.swift
+++ Tinder/LogInViewController.swift
@@ -13,11 +13,13 @@ class LogInViewController: UIViewController {
 
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        errorLabel.isHidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,9 +28,64 @@ class LogInViewController: UIViewController {
     }
     
     @IBAction func logInButtonPressed(_ sender: Any) {
+        
+        let user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        
+        PFUser.logInWithUsername(inBackground: usernameTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error == nil {
+                
+                print("Sign in successful")
+                
+            } else {
+                
+                print("Error signing in. \(error!)")
+                
+                if let currentError = error as NSError? {
+                    
+                    if let errorMessage = currentError.userInfo["error"] as? String {
+                        
+                        self.errorLabel.text = errorMessage
+                        self.errorLabel.isHidden = false
+                        
+                    }
+                }
+            }
+        }
+        
     }
     
     @IBAction func signUpButtonPressed(_ sender: Any) {
+        
+        //should check username and password before continuing
+        
+        let user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                
+                print("Sign up successful")
+                
+            } else {
+                
+                print("Error registering user")
+                
+                if let currentError = error as NSError? {
+                    
+                    if let errorMessage = currentError.userInfo["error"] as? String {
+                        
+                        self.errorLabel.text = errorMessage
+                        self.errorLabel.isHidden = false
+                        
+                    }
+                }
+            }
+        }
+        
     }
     
     
