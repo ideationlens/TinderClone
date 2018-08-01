ddeb9dbb989593c83d978e91765b1922c70d2dde
douglas-putnam <douglasputnam@gmail.com>
2018-08-01 10:28:54 -0400

    Add SettingsViewController and add logout feature

---
+++ Tinder/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  Tinder
+//
+//  Created by Douglas Putnam on 8/1/18.
+//  Copyright © 2018 Douglas Putnam. All rights reserved.
+//
+
+import Parse
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logoutPressed(_ sender: Any) {
+        PFUser.logOut()
+        performSegue(withIdentifier: "logoutSegue", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
