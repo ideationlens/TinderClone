a1246e1df155a04d1792b72e6595dbe3b78885c5
douglas-putnam <douglasputnam@gmail.com>
2018-08-02 02:06:08 -0400

    Add methods to load match candidates and save user's swipes

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -16,14 +16,25 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     @IBOutlet weak var matchButton1: UIButton!
     @IBOutlet weak var matchButton2: UIButton!
     
-    
     var centerPoint = [CGPoint]()
     let centerPointOffset: CGFloat = 8
     let interestMargin: CGFloat = 70
     
+    var matchCandidates = [PFUser]()
+    var userIds = [String]()
+    var candidateCount = 0
+    var candidateCounter = 0
+    var loadImageCounter = 0
+    var nextImage = UIImage()
+    
+    var wasLastImageAtPosition2 = false
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        //Start looking for match candidates
+        loadMatchCandidates()
         
         //Create matchButton gesture
         let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(movebuttonBasedOn(gestureRecognizer:)))
@@ -42,8 +53,11 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         centerPoint.append(CGPoint(x: centerPoint[0].x - centerPointOffset * CGFloat(3)
                                   ,y: centerPoint[0].y - centerPointOffset * CGFloat(3)))
         
+        
+        
         //Setup Nav Bar Items
         setupNavBar()
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -53,66 +67,58 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         matchButton1.center = centerPoint[1]
         matchButton2.center = centerPoint[2]
         
-        print("x: \(matchButton.center.x), y: \(matchButton.center.y)")
+        // Set swipe buttons content mode
+        matchButton.imageView?.contentMode = .scaleAspectFill
+        matchButton1.imageView?.contentMode = .scaleAspectFill
+        matchButton2.imageView?.contentMode = .scaleAspectFill
+        
+//        print("x: \(matchButton.center.x), y: \(matchButton.center.y)")
 //        print("x: \(matchButton1.center.x), y: \(matchButton1.center.y)")
 //        print("x: \(matchButton2.center.x), y: \(matchButton2.center.y)")
         
     }
 
-    //Animate button in Resopnse to Gesture
+    // MARK: - SWIPING METHODS
+    //Animate button in resopnse to swipe gesture
     @objc func movebuttonBasedOn(gestureRecognizer: UIPanGestureRecognizer) {
         
         let changeInPosition = gestureRecognizer.translation(in: view)
         
-        //Move button in response to gesture
+        // Move button in response to gesture
         matchButton.center = CGPoint(x: centerPoint[0].x + changeInPosition.x, y: centerPoint[0].y + changeInPosition.y)
         
-        //Check if button is within the interested/not-interested margin
+        // Check if button is within the interested/not-interested margin
         if (matchButton.center.x < interestMargin) {
             
-            //transform button as it moves further to the left
+            // Transform button as it moves further to the left
             let newAlphaValue = max(0.3,matchButton.center.x / 100 + (1 - interestMargin/100))
-            transformmatchButton(newAlphaValue: newAlphaValue, rotationMultiple: -1)
+            transformMatchButton(newAlphaValue: newAlphaValue, rotationMultiple: -1)
             
         } else if (matchButton.center.x > view.bounds.width - interestMargin) {
             
-            //transform button as it moves further to the right
+            // Transform button as it moves further to the right
             let newAlphaValue = max(0.3,(centerPoint[0].x * 2 - matchButton.center.x) / 100.0 + (1 - interestMargin/100))
-            transformmatchButton(newAlphaValue: newAlphaValue, rotationMultiple: 1)
+            transformMatchButton(newAlphaValue: newAlphaValue, rotationMultiple: 1)
             
         }
 
-        //Check if gesture ended
+        // Check if user let go of image (if gesture ended)
         if (gestureRecognizer.state == UIGestureRecognizerState.ended) {
             
             //Check if button is within the interested/not-interested margin
             if (matchButton.center.x < interestMargin) {
                 
-                //transform button out of view to the left
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.transformmatchButton(newAlphaValue: 0, rotationMultiple: -2)
-                    self.matchButton.center = CGPoint(x: -100,
-                                                     y: self.centerPoint[0].y - 100)
-                }) { (success) in
-                    self.loadNextImage()
-                }
+                rejectMatchCandidate()
             
             } else if (matchButton.center.x > view.bounds.width - interestMargin) {
                 
-                //transform button out of view to the right
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.transformmatchButton(newAlphaValue: 0, rotationMultiple: 2)
-                    self.matchButton.center = CGPoint(x: self.centerPoint[0].x * 2 + 100,
-                                                     y: self.centerPoint[0].y - 100)
-                }) { (success) in
-                    self.loadNextImage()
-                }
+                acceptMatchCandidate()
                 
             } else {
                 
                 //Move button back to original position (function invoked when gesture stops)
                 UIView.animate(withDuration: 0.3) {
-                    self.transformmatchButton(newAlphaValue: 1.0, rotationMultiple: 0)
+                    self.transformMatchButton(newAlphaValue: 1.0, rotationMultiple: 0)
                     self.matchButton.center = self.centerPoint[0]
                 }
                 print("back to center")
@@ -120,9 +126,147 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             }
         }
     }
-
     
-    func transformmatchButton(newAlphaValue: CGFloat, rotationMultiple: CGFloat) {
+    // Method for rejecting match candidates
+    func rejectMatchCandidate () {
+        
+        print("Rejecting candidate \(candidateCounter + 1)")
+        
+        // record user's swipe to the left
+        if let userId = matchCandidates[candidateCounter].objectId {
+            PFUser.current()?.addUniqueObject(userId, forKey: "rejectedCandidates")
+            PFUser.current()?.saveInBackground(block: { (success, error) in
+                if success {
+                    print("Match Candidate \(self.candidateCounter + 1) rejected")
+                } else {
+                    //print("Error encountered while trying to save swipe to left.")
+                    if let parseError = error as NSError? {
+                        if let errorMessage = parseError.userInfo["error"] as? String {
+                            print(errorMessage)
+                        }
+                    }
+                    fatalError("Error encountered while trying to save swipe to left.")
+                }
+            })
+        } else {
+            print("Error while trying to get candidate's objectId")
+        }
+        
+        //transform button out of view to the left
+        print("moving card off screen to left")
+        UIView.animate(withDuration: 0.3, animations: {
+            self.transformMatchButton(newAlphaValue: 0, rotationMultiple: -2)
+            self.matchButton.center = CGPoint(x: -100,
+                                              y: self.centerPoint[0].y - 100)
+        }) { (success) in
+            if success {
+                //show user the next match candidate
+                print("Continuing to next candidate")
+                self.slideImagesForward()
+            }
+        }
+        
+    }
+    
+    // Method for accepting match candidates
+    func acceptMatchCandidate () {
+        
+        print("Accepting candidate \(candidateCounter + 1)")
+        // record user's swipe to the right
+        if let userId = matchCandidates[candidateCounter].objectId {
+            PFUser.current()?.addUniqueObject(userId, forKey: "acceptedCandidates")
+            PFUser.current()?.saveInBackground(block: { (success, error) in
+                if success {
+                    print("Match Candidate \(self.candidateCounter + 1) accepted")
+                } else {
+                    //print("Error encountered while trying to save swipe to right.")
+                    if let parseError = error as NSError? {
+                        if let errorMessage = parseError.userInfo["error"] as? String {
+                            print(errorMessage)
+                        }
+                    }
+                    fatalError("Error encountered while trying to save swipe to right action.")
+                }
+            })
+        } else {
+            print("Error while trying to get candidate's objectId")
+        }
+        
+        //transform rejected button out of view to the right
+        print("Moving candidate off screen to the right")
+        UIView.animate(withDuration: 0.3, animations: {
+            self.transformMatchButton(newAlphaValue: 0, rotationMultiple: 2)
+            self.matchButton.center = CGPoint(x: self.centerPoint[0].x * 2 + 100,
+                                              y: self.centerPoint[0].y - 100)
+        }) { (success) in
+            if success {
+                // show user the next match candidate
+                print("Continuing to next candidate")
+                self.slideImagesForward()
+            }
+        }
+        
+    }
+    
+    //When a card is discarded, make it appear as if the deck is sliding forward into original position
+    func slideImagesForward() {
+        
+        if candidateCount > candidateCounter + 1 {
+            
+            // track which candidate is at the top of the deck
+            candidateCounter += 1
+            print("Up next, candidate \(candidateCounter + 1)")
+        
+            //update top button's (matchButton0) image, position over matchButton1, then set alpha back to 1
+            matchButton.setImage(matchButton1.imageView?.image, for: .normal)
+            matchButton.center = centerPoint[1]
+            transformMatchButton(newAlphaValue: 1, rotationMultiple: 0)
+        
+            //update middle button's (matchButton1) image then position over matchButton
+            if matchButton2.alpha == 0 {
+                print("Setting middle button image to nil")
+                matchButton1.setImage(nil, for: .normal)
+                matchButton1.alpha = 0
+            } else {
+                matchButton1.setImage(matchButton2.imageView?.image, for: .normal)
+            }
+            matchButton1.center = centerPoint[2]
+        
+            //update back button's alpha value to 0, image to new image, then position to centerPoint[3]
+            matchButton2.alpha = 0
+            matchButton2.center = centerPoint[3]
+            if let image = nextMatchCandidateImage() as UIImage? {
+                matchButton2.setImage(image, for: .normal)
+            } else {
+                matchButton2.setImage(nil, for: .normal )
+            }
+        
+            //Animate buttons sliding forward
+            UIView.animate(withDuration: 0.4, animations: {
+                self.matchButton.center = self.centerPoint[0]
+                self.matchButton1.center = self.centerPoint[1]
+                self.matchButton2.center = self.centerPoint[2]
+                if self.wasLastImageAtPosition2 {
+                    print("keeping button2 alpha value set to 0")
+                    self.matchButton2.alpha = 0
+                } else {
+                    self.matchButton2.alpha = 1
+                }
+                
+            }, completion: nil)
+        
+            if candidateCount == loadImageCounter {
+                wasLastImageAtPosition2 = true
+            }
+        } else {
+            print("No new candidates")
+            print("Alert the user that there are no more matches in the area. Try again later")
+        }
+        
+    }
+    
+    //Method to transform button to mimick the motion of
+    func transformMatchButton(newAlphaValue: CGFloat, rotationMultiple: CGFloat) {
         matchButton.alpha = newAlphaValue
         let angle =  CGFloat(1 - newAlphaValue) * CGFloat(Float.pi/16) * rotationMultiple
         let rotation = CGAffineTransform.init(rotationAngle: angle)
@@ -131,35 +275,106 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         matchButton.transform = rotationAndScale
     }
     
-    func loadNextImage() {
+    
+    // MARK: - Load User's Match Candidates Methods
+    
+    func loadMatchCandidates() {
+        
+        print("Starting to load potential matches")
+        guard let query = PFUser.query() else {fatalError("Can not initiate a user query")}
+        
+        //Filter for desired sex
+        if let isInterestedInWomen = PFUser.current()?["isInterestedInWomen"] {
+            query.whereKey("isFemale", equalTo: isInterestedInWomen)
+        }
+        
+        //Filter out users that have already been accepted or rejected
+        var ignoredUsers: [String] = []
 
-        //update top button's (matchButton0) image, position over matchButton1, then set alpha back to 1
-        matchButton.backgroundColor = matchButton1.backgroundColor
-        matchButton.center = centerPoint[1]
-        transformmatchButton(newAlphaValue: 1, rotationMultiple: 0)
+        if let acceptedUsers = PFUser.current()?["acceptedCandidates"] as? [String] {
+            ignoredUsers += acceptedUsers
+        }
+
+        if let rejectedUsers = PFUser.current()?["rejectedCandidates"] as? [String] {
+            ignoredUsers += rejectedUsers
+        }
+
+        let reviewedCandidates = ignoredUsers.count
+        print("Filtering out \(reviewedCandidates) users")
+        query.whereKey("objectId", notContainedIn: ignoredUsers)
         
-        //update middle button's (matchButton1) image then position over matchButton
-        matchButton1.backgroundColor = matchButton2.backgroundColor
-        matchButton1.center = centerPoint[2]
         
-        //update back button's alpha value to 0, image to new image, then position to centerPoint[3]
-        matchButton2.alpha = 0
-        let randomRed = CGFloat(drand48())
-        let randomGreen = CGFloat(drand48())
-        let randomBlue = CGFloat(drand48())
-        matchButton2.backgroundColor = UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1)
-        matchButton2.center = centerPoint[3]
+        //limit how many match candidates can be loaded at one time
+        query.limit = 4
         
-        //Animate buttons sliding forward
-        UIView.animate(withDuration: 0.4, animations: {
-            self.matchButton.center = self.centerPoint[0]
-            self.matchButton1.center = self.centerPoint[1]
-            self.matchButton2.center = self.centerPoint[2]
-            
-            self.matchButton2.alpha = 1
-        }, completion: nil)
+        //Fetch query results and append results to local matchCandidates variable
+        query.findObjectsInBackground { (queryResults, error) in
+            if let objects = queryResults {
+                print("\(objects.count) matches found...")
+                for object in objects {
+                    guard let matchCandidate = object as? PFUser else {fatalError("Query result could not be cast as PFUser")}
+                    self.matchCandidates.append(matchCandidate)
+                    print("Candidate \(object.objectId!) identified")
+                }
+                
+                self.candidateCount = self.matchCandidates.count
+                self.initializeButtonImages()
+                
+            }
+        }
         
     }
+    
+    func initializeButtonImages() {
+        
+        // Load top image
+        if let image0 = nextMatchCandidateImage() {
+            matchButton.setImage(image0, for: .normal)
+        } else {
+            matchButton.alpha = 0
+            //Load message - there are no new matches in your area. Try again later
+            print("Alert the user that there are no more matches in the area. Try again later")
+        }
+        
+        // Load middle image
+        if let image1 = nextMatchCandidateImage() {
+            matchButton1.setImage(image1, for: .normal)
+        } else {
+            matchButton1.alpha = 0
+        }
+        
+        // Load back image
+        if let image2 = nextMatchCandidateImage() {
+            matchButton2.setImage(image2, for: .normal)
+        } else {
+            matchButton2.alpha = 0
+        }
+        
+    }
+    
+    // Method for loading a match candidates images from remote server
+    func nextMatchCandidateImage() -> UIImage? {
+
+        if candidateCount > loadImageCounter {
+            loadImageCounter += 1
+            print("Loading image for candidate \(loadImageCounter)")
+            if let photo = matchCandidates[loadImageCounter - 1]["profilePicture1"] as? PFFile {
+                guard let imageData = try? photo.getData() else {fatalError("Could not download image data")}
+                guard let image = UIImage(data: imageData) else {fatalError("Could not format image data")}
+                print("Image for candidate \(loadImageCounter) loaded successfully")
+                return image
+            } else {
+                print("Candidate \(loadImageCounter) does not have a profile picture.")
+                return UIImage(named: "profile")
+            }
+            
+        } else {
+            print("No more candidates available to load at this time")
+            return nil
+        }
+        
+    }
+    
     
     // MARK: - Nav Bar Methods
     //left navbar button to account center
